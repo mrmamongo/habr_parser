391bf39f6314817b00e5f75c42533a84ada758bc
mrmamongo <mrmamongo@gmail.com>
2022-11-24 16:49:43 +0300

    fix proxies add verbose output

--- UserParser.py
+++ UserParser.py
@@ -4,18 +4,24 @@ from pathlib import Path
 
 import aiofiles.os
 import aiohttp
+import tqdm as tqdm
 from aiocsv import AsyncDictReader
+from aiohttp_socks import ProxyConnector
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 
 class UserParser:
-    def __init__(self, filename: str, dirname: str):
+    def __init__(self, filename: str, dirname: str, proxy: str, verbose: bool = False):
         self.logger: logging.Logger = logging.getLogger(self.__class__.__name__)
+        self.verbose: bool = verbose
         self.dirname: str = dirname
         self.filename: str = filename
         self.session: aiohttp.ClientSession | None = None
         self.file: aiofiles.threadpool = None
         self.parsed_urls: tuple[str] = ()
+        self.user_agent: str = UserAgent().random
+        self.connector = ProxyConnector().from_url(proxy)
 
     @staticmethod
     async def __parse_file(filename: Path) -> str:
@@ -24,7 +30,7 @@ class UserParser:
                 return line[:-1]
 
     async def __aenter__(self):
-        self.session = aiohttp.ClientSession()
+        self.session = aiohttp.ClientSession(connector=self.connector)
         self.file = await aiofiles.open(self.filename, "r", encoding='utf-8')
         if Path(self.dirname).exists():
             self.parsed_urls = tuple(
@@ -34,26 +40,34 @@ class UserParser:
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.connector.close()
         if self.session is not None:
             await self.session.close()
         await self.file.close()
 
-    async def __parse_user(self, url: str) -> None:
-        page = await self.session.get(url)
-        html_page: str = await page.text()
-        name: str = BeautifulSoup(html_page, 'lxml').title.get_text().split(' — ')[0]
-        await self.__dump_user(url, name, html_page)
+    async def __parse_user(self, url: str) -> bool:
+        try:
+            page = await self.session.get(url, headers={"user-agent": self.user_agent})
+            html_page: str = await page.text()
+            name: str = BeautifulSoup(html_page, 'lxml').title.get_text().split(' — ')[0]
+            await self.__dump_user(url, name, html_page)
+            return True
+        except Exception as e:
+            self.logger.error(e)
+            return False
 
     async def parse_users(self) -> int:
         try:
-            tasks = []
-            async for row in AsyncDictReader(self.file, fieldnames=['url']):
-                if row['url'] != 'url' and row['url'] not in self.parsed_urls and row['url'] not in self.parsed_urls:
-                    tasks.append(asyncio.create_task(self.__parse_user(row['url'])))
-            if len(tasks) > 0:
-                done_tasks, _ = await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
-                return len(done_tasks)
-            return 0
+            tasks = [asyncio.create_task(self.__parse_user(row['url'])) async for row in
+                     AsyncDictReader(self.file, fieldnames=['url'])
+                     if row['url'] != 'url' and row['url'] not in self.parsed_urls]
+            if self.verbose:
+                responses = [await f
+                             for f in tqdm.tqdm(asyncio.as_completed(tasks), total=len(tasks))]
+            else:
+                responses = await asyncio.gather(*tasks)
+
+            return len(list(filter(bool, responses)))
         except Exception as e:
             self.logger.error(e)
             return 0
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import asyncio
 import logging
 from argparse import ArgumentParser
-
-import aiorun
 
 from UserParser import UserParser
 
@@ -10,20 +9,24 @@ async def main():
     argparser = ArgumentParser()
     argparser.add_argument('-i', '--input', default='habr.csv', type=str, help="Csv file with urls")
     argparser.add_argument('-o', '--output', default='out', type=str, help="Output directory")
-    argparser.add_argument('-l', '--log', default="INFO", type=str, help="Log level",
+    argparser.add_argument('-l', '--log', default="DEBUG", type=str, help="Log level",
                            choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'])
+    argparser.add_argument('-p', '--proxy', default='socks5://testvicky2:2a31eb@193.23.50.245:10490', type=str,
+                           help="Proxy server")
+    argparser.add_argument('-v', '--verbose', action='store_true', help="Verbose run")
     args = argparser.parse_args()
     logging.basicConfig(
+        filename='parser.log',
         level=args.log,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
 
     logger = logging.getLogger(__name__)
-    async with UserParser(args.input, args.output) as parser:
+    async with UserParser(args.input, args.output, args.proxy, args.verbose) as parser:
         parsed = await parser.parse_users()
-        logger.debug("Done. Parsed users: %s", parsed)
+        logger.info("Done. Parsed users: %s", parsed)
         logger.debug("Already parsed files: %s", parser.parsed_urls)
 
 
 if __name__ == '__main__':
-    aiorun.run(main())
+    asyncio.run(main())
